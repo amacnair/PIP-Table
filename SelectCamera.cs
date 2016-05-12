e23a98e04d3d502ae73c8368833e102870b7e681
amacnair <alex@alexmacnair.com>
2016-05-12 06:47:08 -0400

    New Camera Switch and Debug Code for Screen Share

--- SelectCamera.cs
+++ SelectCamera.cs
@@ -21,23 +21,66 @@ using System.Collections;
 
 public class SelectCamera : MonoBehaviour {
 
+	public GameObject camText1;
+	public GameObject camText2;
+	public GameObject camText3;
+	public GameObject camText4;
+
 	public Camera camera1;
 	public Camera camera2;
 	public Camera camera3;
 	public Camera camera4;
 
+	private int deltaFrames = 0;
+
+	private bool helpTextEnabled = false;
+
 
 	// Use this for initialization
 	void Start () {
+
 		camera1.enabled = true;
 		camera2.enabled = false;
 		camera3.enabled = false;
 		camera4.enabled = false;
+
+		camText1.SetActive(false);
+		camText2.SetActive(false);
+		camText3.SetActive(false);
+		camText4.SetActive(false);
+		helpTextEnabled = false;
+
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey("f1"))
+		++deltaFrames;
+		if (Input.GetKey("c") && (deltaFrames > 8)) {
+			Debug.Log("Enabing help text");
+			if (helpTextEnabled == false) {
+
+				camText1.SetActive(true);
+				camText2.SetActive(true);
+				camText3.SetActive(true);
+				camText4.SetActive(true);
+				helpTextEnabled = true;
+				deltaFrames = 0;
+			}
+			else {
+				camText1.SetActive(false);
+				camText2.SetActive(false);
+				camText3.SetActive(false);
+				camText4.SetActive(false);
+				helpTextEnabled = false;
+				deltaFrames = 0;
+			}
+
+
+		}
+
+
+		if (Input.GetKey("1"))
 		{
 			Debug.Log("Camera 1 Active");
 			camera1.enabled = true;
@@ -46,7 +89,7 @@ public class SelectCamera : MonoBehaviour {
 			camera4.enabled = false;
 
 		}
-		if (Input.GetKey("f2"))
+		if (Input.GetKey("2"))
 		{
 			Debug.Log("Camera 2 Active");
 			camera2.enabled = true;
@@ -55,7 +98,7 @@ public class SelectCamera : MonoBehaviour {
 			camera4.enabled = false;
 
 		}
-		if (Input.GetKey("f3"))
+		if (Input.GetKey("3"))
 		{
 			Debug.Log("Camera 3 Active");
 			camera3.enabled = true;
@@ -64,7 +107,7 @@ public class SelectCamera : MonoBehaviour {
 			camera4.enabled = false;
 
 		}
-		if (Input.GetKey("f4"))
+		if (Input.GetKey("4"))
 		{
 			Debug.Log("Camera 4 Active");
 			camera4.enabled = true;
